ac6ba2bfa8e5301dd0b23c817c5f71d31074ee0f
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-13 14:42:19 +0300

    Update loader.py

--- src/profiles/management/commands/loader.py
+++ src/profiles/management/commands/loader.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
+
 from cfehome import utils as cfehome_utils
+from movies.models import Movie
 
 User = get_user_model()
 
@@ -11,23 +13,27 @@ class Command(BaseCommand):
         parser.add_argument("--movies", action='store_true', default=False)
         parser.add_argument("--users", action='store_true', default=False)
         parser.add_argument("--show-total", action='store_true', default=False)
-        
+    
     def handle(self, *args, **options):
         count = options.get('count')
         show_total = options.get('show_total')
         load_movies = options.get('movies')
         generate_users = options.get('users')
         if load_movies:
-            movies_dataset = cfehome_utils.load_movie_data(limit=count)
+            movie_dataset = cfehome_utils.load_movie_data(limit=count)
+            movies_new = [Movie(**x) for x in movie_dataset]
+            movies_bulk = Movie.objects.bulk_create(movies_new, ignore_conflicts=True)
+            print(f"New movies: {len(movies_bulk)}")
+            if show_total:
+                print(f"Total movies: {Movie.objects.count()}")
         if generate_users:
             profiles = cfehome_utils.get_fake_profiles(count=count)
-            # print(f'hello {User.objects.count()}')
             new_users = []
             for profile in profiles:
                 new_users.append(
-                    User.objects.create(**profile)
+                    User(**profile)
                 )
             user_bulk = User.objects.bulk_create(new_users, ignore_conflicts=True)
-            print(f"new users created {len(user_bulk)}")
+            print(f"New users: {len(user_bulk)}")
             if show_total:
                 print(f"Total users: {User.objects.count()}")
