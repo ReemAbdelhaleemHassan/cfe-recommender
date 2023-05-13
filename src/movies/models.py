7b5d6d806d77df48e4909539cc37509831a86001
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-13 19:11:18 +0300

    Handling Duplicate Ratings with Signals

--- src/movies/models.py
+++ src/movies/models.py
@@ -7,17 +7,21 @@ from ratings.models import Rating
 
 RATING_CALC_TIME = 1
 
-# Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=120, unique=True)
     overview = models.TextField()
     release_date = models.DateField(blank=True, null=True, auto_now=False, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now_add=True)
-    ratings = GenericRelation(Rating) # queryset 
+    ratings = GenericRelation(Rating) # queryset
     rating_last_updated = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
     rating_count = models.IntegerField(blank=True, null=True)
     rating_avg = models.DecimalField(decimal_places=2, max_digits=5, blank=True, null=True) # 5.00, 0.00
-    
+
+    def __str__(self):
+        if not self.release_date:
+            return f"{self.title}"
+        return f"{self.title} ({self.release_date.year})"
+
     def rating_avg_display(self):
         now = timezone.now()
         if not self.rating_last_updated:
@@ -31,7 +35,7 @@ class Movie(models.Model):
     
     def calculate_ratings_avg(self):
         return self.ratings.all().avg()
-    
+
     def calculate_rating(self, save=True):
         rating_avg = self.calculate_ratings_avg()
         rating_count = self.calculate_ratings_count()
