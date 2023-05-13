4342d1abfa162dda85a4bbdd615aa66bc1b15be8
ReemAbdelhaleemHassan <102296115+ReemAbdelhaleemHassan@users.noreply.github.com>
2023-05-13 14:42:28 +0300

    Update utils.py

--- src/cfehome/utils.py
+++ src/cfehome/utils.py
@@ -1,17 +1,45 @@
 from faker import Faker
 from pprint import pprint
 import csv
+import csv
+import datetime
+
+from pprint import pprint
 from django.conf import settings
+
+from faker import Faker
 
 MOVIE_METADATA_CSV = settings.DATA_DIR / "movies_metadata.csv"
 
-def load_movie_data(limit=2):
+def validate_date_str(date_text):
+    try:
+        datetime.datetime.strptime(date_text, "%Y-%m-%d")
+    except:
+        return None
+    return date_text
+
+def load_movie_data(limit=1):
     with open(MOVIE_METADATA_CSV, newline='', encoding="utf8") as csvfile:
         reader = csv.DictReader(csvfile)
+        dataset = []
         for i, row in enumerate(reader):
-            pprint(row)
-            if i+1>limit:
+            _id = row.get("id")
+            try:
+                _id = int(_id)
+            except:
+                _id = None
+            release_date = validate_date_str(row.get('release_date'))
+            data = {
+                "id": _id,
+                "title": row.get('title'),
+                "overview": row.get("overview"),
+                "release_date": release_date
+            }
+            dataset.append(data)
+            if i + 1 > limit:
                 break
+        return dataset
+
 
 def get_fake_profiles(count=10):
     fake = Faker()
@@ -20,14 +48,12 @@ def get_fake_profiles(count=10):
         profile = fake.profile()
         data = {
             "username": profile.get('username'),
-            "email": profile.get("mail"),
-            # "password": make_password(fake.password(length=15)),
-            "is_active": True,
+            "email": profile.get('mail'),
+            "is_active": True
         }
         if 'name' in profile:
             fname, lname = profile.get('name').split(" ")[:2]
             data['first_name'] = fname
             data['last_name'] = lname
         user_data.append(data)
-        # print(fake.profile())
     return user_data
